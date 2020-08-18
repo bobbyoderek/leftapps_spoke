106bb30bef9a52e31c17ea64a5e7574a839f081a
Eric Mayefsky <bobbyoderek@users.noreply.github.com>
2020-08-18 13:50:32 -0400

    remove print statements

--- src/workers/errored-message-sender.js
+++ src/workers/errored-message-sender.js
@@ -1,13 +1,10 @@
 import { erroredMessageSender } from "./job-processes";
 console.log("Kicking off an errored message resend run");
 setInterval(
-  console
-    .log("Set interval starting")
-    .then(
-      erroredMessageSender().catch(err => {
-        console.log(err);
-      })
-    )
+  erroredMessageSender()
+    .catch(err => {
+      console.log(err);
+    })
     .then(() => console.log("Set interval ending")),
   30000
 );
