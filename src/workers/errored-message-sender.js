57d32287977bf91d204696fa828e42f4f6bd0499
Eric Mayefsky <bobbyoderek@users.noreply.github.com>
2020-08-17 21:28:10 -0400

    add sleep and process exit to errored message sender

--- src/workers/errored-message-sender.js
+++ src/workers/errored-message-sender.js
@@ -1,6 +1,13 @@
+function sleep(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
 import { erroredMessageSender } from "./job-processes";
 console.log("Kicking off an errored message resend run");
 erroredMessageSender().catch(err => {
   console.log(err);
 });
 console.log("Finishing up an errored message resend run");
+await sleep(10000);
+process.exit();
