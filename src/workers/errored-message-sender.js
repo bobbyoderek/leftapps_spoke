e92559a9eaf1f2d1dcba5c657700f428cf3f1606
Eric Mayefsky <bobbyoderek@users.noreply.github.com>
2020-08-18 09:41:53 -0400

    try await errored message sender

--- src/workers/errored-message-sender.js
+++ src/workers/errored-message-sender.js
@@ -1,18 +1,10 @@
 import { erroredMessageSender } from "./job-processes";
 console.log("Kicking off an errored message resend run");
-erroredMessageSender()
+await erroredMessageSender()
   .catch(err => {
     console.log(err);
   })
   .then(() => console.log("finished with the sender"));
 console.log("Finishing up an errored message resend run");
 
-function sleep(millis) {
-  return new Promise(resolve => setTimeout(resolve, millis));
-}
-console.log("Pre-sleep");
-sleep(100000).then(() => {
-  console.log("Sleep has elapsed, shutting down");
-});
-console.log("Post-sleep");
 process.exit();
